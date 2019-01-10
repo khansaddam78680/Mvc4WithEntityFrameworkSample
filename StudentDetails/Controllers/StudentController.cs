ab924b8ab018d1c13300a2e36640eb8934e73cfa
Saddam Khan <Saddam.Khan@ASCKPS.IN>
2019-01-10 12:26:01 -0800

    Uploaded Code To git

---
+++ StudentDetails/Controllers/StudentController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentDetails.Models;
+using StudentDetails.ViewModels;
+
+namespace StudentDetails.Controllers
+{
+    [HandleError]
+    public class StudentController : Controller
+    {
+        private StudentDBContext _db;
+        public StudentController()
+        {
+            this._db = new StudentDBContext();
+        }
+        //
+        // GET: /Student/
+        [ActionName("StudentsDetail")]
+        public ActionResult Index()
+        {
+            List<StudentDetailViewModel> studentDetails = new List<StudentDetailViewModel>();
+            var data = _db.StudentDetails;
+            foreach (var item in data)
+            {
+                double totalmarks = 300;
+                double obtainedmarks = item.English + item.Science + item.Computer;
+                string grade = "";
+                double percent;
+                percent = (obtainedmarks * 100) / totalmarks;
+                percent = Math.Round(percent, 2);
+                if (percent >= 35 && percent < 40)
+                {
+                    grade = "Pass Class";
+                }
+                else if (percent >= 40 && percent < 60)
+                {
+                    grade = "Second Class";
+                }
+                else if (percent >= 60 && percent < 70)
+                {
+                    grade = "First Class";
+                }
+                else if (percent >= 70 && percent <= 100)
+                {
+                    grade = "Distinction";
+                }
+
+                studentDetails.Add(new StudentDetailViewModel
+                {
+                    StudentName = item.StudentName,
+                    English = item.English,
+                    Science = item.Science,
+                    Computer = item.Computer,
+                    Year = item.Year,
+                    Percentage = (decimal)percent,
+                    Grade = grade
+                });
+            }
+            return View(studentDetails);
+        }
+
+        [HttpGet, ActionName("NewStudentData")]
+        public ActionResult InsertNewStudentDetail()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("NewStudentData")]
+        public ActionResult InsertNewStudentDetail(StudentDetail studentDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.StudentDetails.AddObject(studentDetails);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ StudentDetails/Models/StudentDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentDetails.Models
+{
+    [MetadataType(typeof(StudentDetailMetaData))]
+    public partial class StudentDetail
+    {
+
+    }
+    public class StudentDetailMetaData
+    {
+        [Required(ErrorMessage = "Student Name is Required")]
+        [StringLength(50, MinimumLength = 5)]
+        [Display(Name="Student Name")]
+        public string StudentName { get; set; }
+
+        [Required(ErrorMessage = "English Marks is Required")]
+        [Range(0, 100)]
+        public int English { get; set; }
+
+        [Required(ErrorMessage = "Science Marks is Required")]
+        [Range(0, 100)]
+        public int Science { get; set; }
+
+        [Required(ErrorMessage = "Computer Marks is Required")]
+        [Range(0, 100)]
+        public int Computer { get; set; }
+
+        [Required(ErrorMessage = "Year is Required")]
+        [Range(0000, 9999)]
+        public int Year { get; set; }
+        //public decimal Percentage { get; set; }
+        //public string Grade { get; set; }
+    }
+}
